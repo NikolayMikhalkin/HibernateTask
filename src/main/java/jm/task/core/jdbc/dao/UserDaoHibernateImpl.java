aed85df2384aee7a59eb2d04cf327711c86efa09
Михалкин Николай Владимирович <mikhalkin@mmugb7.local>
2024-02-15 16:09:54 +0400

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,34 +15,98 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            String sql = "CREATE TABLE IF NOT EXISTS users (" +
+
+                    "  id BIGINT NOT NULL AUTO_INCREMENT," +
+
+                    "  name VARCHAR(50)," +
+
+                    "  lastName VARCHAR(50)," +
+
+                    "  age TINYINT," +
+
+                    "  PRIMARY KEY (id));";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) { transaction.rollback(); }
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            String sql = "DROP TABLE IF EXISTS users";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) { transaction.rollback(); }
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
 
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) { transaction.rollback(); }
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) { transaction.rollback(); }
+            e.getStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            List<User> users = new ArrayList<>();
+            return users = session.createCriteria(User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getNewSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.clear();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) { transaction.rollback(); }
+            e.getStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+//import java.lang.module.Configuration;
+//import javax.imageio.spi.ServiceRegistry;
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
 
@@ -8,6 +19,9 @@ public class Util {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/kata";
     private static final String NAME = "root";
     private static final String PSW = "root";
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getNewConnection() {
 
@@ -22,4 +36,35 @@ public class Util {
 
         return connection;
     }
+
+    public static SessionFactory getNewSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DB_DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, NAME);
+                settings.put(Environment.PASS, PSW);
+                settings.put(Environment.DIALECT, DIALECT);
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
 }
